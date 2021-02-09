9332a202fce398439556a11df0aaa3ad39174453
stevenacz <scoailazaa@gmail.com>
2021-02-09 15:56:56 -0500

    all about projects added

--- src/components/pages/ProjectsScreen.js
+++ src/components/pages/ProjectsScreen.js
@@ -1,10 +1,60 @@
 import React from 'react'
+import angryBirds from '../../img/angry-birds.png'
+import ProjectCard from '../projects/ProjectCard'
 
 const ProjectsScreen = () => {
+  let projects = [
+    {
+      id: '12a',
+      name: 'Angry Birds',
+      img: angryBirds,
+      skills: ['HTML', 'CSS'],
+      webpage: 'https://stevenacz.github.io/week2-team1/',
+      github: 'https://github.com/StevenACZ/week2-team1'
+    },
+    {
+      id: '12a',
+      name: 'Angry Birds',
+      img: angryBirds,
+      skills: ['HTML', 'CSS'],
+      webpage: 'https://stevenacz.github.io/week2-team1/',
+      github: 'https://github.com/StevenACZ/week2-team1'
+    },
+    {
+      id: '12a',
+      name: 'Angry Birds',
+      img: angryBirds,
+      skills: ['HTML', 'CSS'],
+      webpage: 'https://stevenacz.github.io/week2-team1/',
+      github: 'https://github.com/StevenACZ/week2-team1'
+    },
+    {
+      id: '12a',
+      name: 'Angry Birds',
+      img: angryBirds,
+      skills: ['HTML', 'CSS'],
+      webpage: 'https://stevenacz.github.io/week2-team1/',
+      github: 'https://github.com/StevenACZ/week2-team1'
+    },
+  ]
+
   return (
-    <div>
-      <h1>Projects Section</h1>
-    </div>
+    <section className="projects-section">
+      <div className="hero">
+        <h2>Projects</h2>
+      </div>
+
+      <section className="projects-container">
+        {
+          projects.map( project => (
+            <ProjectCard
+              key={ project.id }
+              { ...project }
+            />
+          ))
+        }
+      </section>
+    </section>
   )
 }
 
---
+++ src/components/projects/ProjectCard.js
@@ -0,0 +1,33 @@
+import React from 'react'
+
+const ProjectCard = ({
+  img,
+  skills,
+  webpage,
+  github
+}) => {
+  return (
+    <div className="project-card">
+      <div className="project-img">
+        <img src={ img } />
+      </div>
+
+      <div className="project-information">
+        <div className="project-skills">
+          {
+            skills.map( skill => (
+              <span>{ skill }</span>  
+            ))
+          }
+        </div>
+
+        <div className="project-links">
+          <a href={ webpage } target="_blank">Web page</a>
+          <a href={ github } target="_blank">GitHub</a>
+        </div>
+      </div>
+    </div>
+  )
+}
+
+export default ProjectCard
