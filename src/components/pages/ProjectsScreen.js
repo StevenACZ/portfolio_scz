ccdf3b07cf025feaef49dd110b51badd44f190bd
stevenacz <scoailazaa@gmail.com>
2021-02-09 16:59:55 -0500

    improved

--- src/components/pages/ProjectsScreen.js
+++ src/components/pages/ProjectsScreen.js
@@ -1,6 +1,18 @@
 import React from 'react'
-import angryBirds from '../../img/angry-birds.png'
 import ProjectCard from '../projects/ProjectCard'
+
+import angryBirds from '../../img/angry-birds.png'
+import angryBirdsStories from '../../img/angry-birds-stories.png'
+import todoListJs from '../../img/todolistjs.png'
+import colorGame from '../../img/color-game.png'
+import a11y from '../../img/a11y.png'
+import chairdnb from '../../img/chairdnb.png'
+import blogSass from '../../img/blog-sass.png'
+import elVeraz from '../../img/el-veraz.png'
+import natoursPage from '../../img/natours-page.png'
+import pigGame from '../../img/pig-game.png'
+import portfolioV2 from '../../img/portfolio-v2.png'
+import portfolioV1 from '../../img/portfolio-v1.png'
 
 const ProjectsScreen = () => {
   let projects = [
@@ -13,28 +25,92 @@ const ProjectsScreen = () => {
       github: 'https://github.com/StevenACZ/week2-team1'
     },
     {
-      id: '12a',
-      name: 'Angry Birds',
-      img: angryBirds,
-      skills: ['HTML', 'CSS'],
-      webpage: 'https://stevenacz.github.io/week2-team1/',
-      github: 'https://github.com/StevenACZ/week2-team1'
+      id: '121a',
+      name: 'ToDoListJS',
+      img: todoListJs,
+      skills: ['HTML', 'CSS', 'JavaScript'],
+      webpage: 'https://stevenacz.github.io/ToDo-List-JS/',
+      github: 'https://github.com/StevenACZ/ToDo-List-JS'
     },
     {
-      id: '12a',
-      name: 'Angry Birds',
-      img: angryBirds,
-      skills: ['HTML', 'CSS'],
-      webpage: 'https://stevenacz.github.io/week2-team1/',
-      github: 'https://github.com/StevenACZ/week2-team1'
+      id: '12adasd',
+      name: 'Color Game',
+      img: colorGame,
+      skills: ['HTML', 'CSS', 'JavaScript'],
+      webpage: 'https://stevenacz.github.io/color-game/',
+      github: 'https://github.com/StevenACZ/color-game'
     },
     {
-      id: '12a',
-      name: 'Angry Birds',
-      img: angryBirds,
+      id: '12a123s',
+      name: 'Angry Birds Stories',
+      img: angryBirdsStories,
       skills: ['HTML', 'CSS'],
-      webpage: 'https://stevenacz.github.io/week2-team1/',
-      github: 'https://github.com/StevenACZ/week2-team1'
+      webpage: 'https://stevenacz.github.io/html-css-individual-StevenACZ/',
+      github: 'https://github.com/StevenACZ/html-css-individual-StevenACZ'
+    },
+    {
+      id: '12a123222s',
+      name: 'a11y',
+      img: a11y,
+      skills: ['HTML', 'CSS'],
+      webpage: 'https://stevenacz.github.io/component-library-animations-a11y-StevenACZ/',
+      github: 'https://github.com/StevenACZ/component-library-animations-a11y-StevenACZ'
+    },
+    {
+      id: '12a123lldws',
+      name: 'Chairdnb',
+      img: chairdnb,
+      skills: ['HTML', 'CSS'],
+      webpage: 'https://stevenacz.github.io/week1-team5/',
+      github: 'https://github.com/StevenACZ/week1-team5'
+    },
+    {
+      id: '1',
+      name: 'Blog contact',
+      img: blogSass,
+      skills: ['HTML', 'CSS', 'SASS'],
+      webpage: 'https://stevenacz.github.io/BlogSass/',
+      github: 'https://github.com/StevenACZ/BlogSass'
+    },
+    {
+      id: '2',
+      name: 'El veraz',
+      img: elVeraz,
+      skills: ['HTML', 'CSS', 'SASS', 'JavaScript'],
+      webpage: 'https://stevenacz.github.io/Elveraz/',
+      github: 'https://github.com/StevenACZ/Elveraz'
+    },
+    {
+      id: '99',
+      name: 'Natours Page',
+      img: natoursPage,
+      skills: ['HTML', 'CSS', 'SASS'],
+      webpage: 'https://stevenacz.github.io/Natours-Page/',
+      github: 'https://github.com/StevenACZ/Natours-Page'
+    },
+    {
+      id: '12p342',
+      name: 'Pig game',
+      img: pigGame,
+      skills: ['HTML', 'CSS', 'JavaScript'],
+      webpage: 'https://stevenacz.github.io/Pig-Game/',
+      github: 'https://github.com/StevenACZ/Pig-Game'
+    },
+    {
+      id: '12p342s22323',
+      name: 'Portfolio',
+      img: portfolioV2,
+      skills: ['HTML', 'CSS', 'SASS', 'React', 'Js'],
+      webpage: 'https://stevenacz.herokuapp.com/',
+      github: 'https://github.com/StevenACZ/portfolio_scz'
+    },
+    {
+      id: 'oli23',
+      name: 'Portfolio v1',
+      img: portfolioV1,
+      skills: ['HTML', 'CSS', 'SASS', 'Js'],
+      webpage: 'https://stevenacz.github.io/Portfolio_Dev/',
+      github: 'https://github.com/StevenACZ/Portfolio_Dev'
     },
   ]
 
