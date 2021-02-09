60d045a2331d1ebbf53bcf6ce4ff7eb978fe98d4
stevenacz <scoailazaa@gmail.com>
2021-02-09 13:50:32 -0500

    all about contact me added

--- src/components/pages/ContactScreen.js
+++ src/components/pages/ContactScreen.js
@@ -1,10 +1,67 @@
-import React from 'react'
+import React, { useState } from 'react'
 
 const ContactScreen = () => {
+  const [name, setName] = useState('');
+  const [email, setEmail] = useState('');
+  const [subject, setSubject] = useState('');
+  const [messageBody, setMessageBody] = useState('');
+
+  let emailTo = `mailto:scoailazaa@gmail.com?&subject=${subject}&body=${messageBody}`
+
+  const resetContactForm = () => {
+    setName('');
+    setEmail('');
+    setSubject('');
+    setMessageBody('');
+  }
+
   return (
-    <div>
-      <h1>Contact Section</h1>
-    </div>
+    <section className="contact-section">
+      {/* HERO - CONTACT ME */}
+      <div className="hero">
+        <h2>Contact me</h2>
+
+        <p>
+          Hello, if you have a request or question,<br/> don't hesitate to contact me using below form either.
+        </p>
+
+        <div className="contact-form">
+          <input
+            onChange={ (e) => { setName(e.target.value) } }
+            className="name"
+            value={name}
+            type="text"
+            placeholder="Name"
+          />
+          <input
+            onChange={ (e) => { setEmail(e.target.value) } }
+            className="email"
+            value={email}
+            type="text"
+            placeholder="Email"
+          />
+          <input
+            onChange={ (e) => { setSubject(e.target.value) } }
+            value={subject}
+            className="subject"
+            type="text"
+            placeholder="Subject"
+          />
+          <textarea
+            onChange={ (e) => { setMessageBody(e.target.value) } }
+            value={messageBody}
+            className="message"
+            placeholder="Message"
+            cols="30"
+            rows="5"
+          ></textarea>
+        </div>
+
+        <div className="submit-container">
+          <a className="submit" href={emailTo} onClick={ resetContactForm } >Send</a>
+        </div>
+      </div>
+    </section>
   )
 }
 
