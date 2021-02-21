49ebdce444d2595d0ed82e21e5c7f96a691a4d5d
stevenacz <scoailazaa@gmail.com>
2021-02-21 18:05:22 -0500

    React ui

--- src/data/projects.js
+++ src/data/projects.js
@@ -12,15 +12,24 @@ import portfolioV2 from '../img/portfolio-v2.png'
 import portfolioV1 from '../img/portfolio-v1.png'
 import expenseTracker from '../img/expense-tracker.png'
 import reactUi from '../img/react-ui.png'
+import deliverable from '../img/deliverable.png'
 
 export let projects = [
+  {
+    id: '9999222111',
+    name: 'Deliverable',
+    img: deliverable,
+    skills: ['HTML', 'CSS', 'SASS', 'JavaScript', 'React'],
+    webpage: 'https://rappi-fake-react.herokuapp.com/',
+    github: 'https://github.com/StevenACZ/rappi-fake-react'
+  },
   {
     id: '1212a23213123213123123',
     name: 'React UI',
     img: reactUi,
     skills: ['HTML', 'CSS', 'SASS', 'JavaScript', 'React'],
     webpage: 'https://react-ui-acz.herokuapp.com/',
-    github: 'https://github.com/StevenACZ/expense-tracker-react'
+    github: 'https://github.com/StevenACZ/ReactUI'
   },
   {
     id: '12a23213123213123123',
