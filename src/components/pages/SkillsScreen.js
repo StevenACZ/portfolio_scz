67f087c6aad5ab4a03d703b96742e46732e4aabe
stevenacz <scoailazaa@gmail.com>
2021-09-17 19:42:32 -0500

    graphql and postgresql added ruby and ruby on rails removed

--- src/components/pages/SkillsScreen.js
+++ src/components/pages/SkillsScreen.js
@@ -1,94 +1,84 @@
 import React from 'react';
 
 const SkillsScreen = () => {
-  return (
-    <section className="skills-section">
-      {/* HERO - SKILLS */}
-      <div className="hero">
-        <h2>Skills & Experience</h2>
-        <p>
-          The main area of my expertise is front end development
-          <br />
-          <br />
-          HTML, CSS, JS, building small and medium web apps with React
-          <br />
-          features, animations, and coding interactive layouts.
-          <br />
-          <br />
-          I have also a little full-stack developer experience
-          <br />
-          with (Ruby and Ruby on Rails)
-          <br />
-          <br />
-          Visit my{' '}
-          <a href="https://www.linkedin.com/in/stevenacz/" target="_blank">
-            Linkeding
-          </a>{' '}
-          profile for more details.
-        </p>
-      </div>
+	return (
+		<section className="skills-section">
+			{/* HERO - SKILLS */}
+			<div className="hero">
+				<h2>Skills & Experience</h2>
+				<p>
+					The main area of my expertise is front end development
+					<br />
+					<br />
+					HTML, CSS, JS, building small and medium web apps with React
+					<br />
+					features, animations, and coding interactive layouts.
+					<br />
+					<br />
+					Visit my{' '}
+					<a href="https://www.linkedin.com/in/stevenacz/" target="_blank">
+						Linkeding
+					</a>{' '}
+					profile for more details.
+				</p>
+			</div>
 
-      <div className="skills">
-        <div className="skill-range beginner">
-          <div>beginner</div>
-        </div>
-        <div className="skill-range elementary">
-          <div>elementary</div>
-        </div>
-        <div className="skill-range intermediate">
-          <div>intermediate</div>
-        </div>
-        <div className="skill-range advanced">
-          <div>advanced</div>
-        </div>
+			<div className="skills">
+				<div className="skill-range beginner">
+					<div>beginner</div>
+				</div>
+				<div className="skill-range elementary">
+					<div>elementary</div>
+				</div>
+				<div className="skill-range intermediate">
+					<div>intermediate</div>
+				</div>
+				<div className="skill-range advanced">
+					<div>advanced</div>
+				</div>
 
-        <div className="skill">
-          <p>HTML</p>
-          <div className="experience-bar html"></div>
-        </div>
+				<div className="skill">
+					<p>HTML</p>
+					<div className="experience-bar html"></div>
+				</div>
 
-        <div className="skill">
-          <p>CSS</p>
-          <div className="experience-bar css"></div>
-        </div>
+				<div className="skill">
+					<p>CSS / SASS</p>
+					<div className="experience-bar css"></div>
+				</div>
 
-        <div className="skill">
-          <p>JavaScript</p>
-          <div className="experience-bar js"></div>
-        </div>
+				<div className="skill">
+					<p>JavaScript</p>
+					<div className="experience-bar js"></div>
+				</div>
 
-        <div className="skill">
-          <p>TypeScript</p>
-          <div className="experience-bar ts"></div>
-        </div>
+				<div className="skill">
+					<p>TypeScript</p>
+					<div className="experience-bar ts"></div>
+				</div>
 
-        <div className="skill">
-          <p>React</p>
-          <div className="experience-bar react"></div>
-        </div>
+				<div className="skill">
+					<p>React</p>
+					<div className="experience-bar react"></div>
+				</div>
 
-        <div className="skill">
-          <p>Node.js</p>
-          <div className="experience-bar nodejs"></div>
-        </div>
+				<div className="skill">
+					<p>Node.js</p>
+					<div className="experience-bar nodejs"></div>
+				</div>
 
-        <div className="skill">
-          <p>MongoDB</p>
-          <div className="experience-bar mongoDb"></div>
-        </div>
+				<div className="skill">
+					<p>GraphQL</p>
+					<div className="experience-bar graphql"></div>
+				</div>
 
-        <div className="skill">
-          <p>Ruby</p>
-          <div className="experience-bar ruby"></div>
-        </div>
-
-        <div className="skill">
-          <p>Ruby on Rails</p>
-          <div className="experience-bar rubyonrails"></div>
-        </div>
-      </div>
-    </section>
-  );
+				<div className="skill">
+					<p>PostgreSQL</p>
+					<div className="experience-bar postgresql"></div>
+				</div>
+			</div>
+		</section>
+	);
 };
 
 export default SkillsScreen;
